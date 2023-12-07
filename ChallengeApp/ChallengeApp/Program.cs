6418f7d9021272b5a8afdfb79e8560c17e236720
Anna Lukaszczyk <normalnie4@tlen.pl>
2023-12-07 18:03:03 +0100

    data and "if/else" condition added

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+string sex = "kobieta";
+int age = 28;
+
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat.");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33.");
+}
+else if (sex != "kobieta" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna.");
+}
+else
+{
+    Console.WriteLine("Brak osób spełniających warunki.");
+}
