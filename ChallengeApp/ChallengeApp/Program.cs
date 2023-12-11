d52d13b00fe3ce6513f2a166d716d4153b3c3bb3
Anna Lukaszczyk <normalnie4@tlen.pl>
2023-12-11 13:07:11 +0100

    Jak wyliczyć najwyższy wynik.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,39 +1,53 @@
-﻿User user1 = new User("Ania", "sasdf");
-User user2 = new User("Marek", "sasdf");
+﻿using ChallengeApp;
 
-user1.AddScore(5);
-user1.AddScore(2);
-user2.AddScore(3);
-user2.AddScore(6);
-var result1 = user1.Result;
-var result2 = user2.Result;
-Console.WriteLine("Wynik dla user1: " + result1);
-Console.WriteLine("Wynik dla user2: " + result2);
-var name = User.GameName;
-Console.WriteLine(name);
-class User
+Employee employee1 = new Employee("Damian", "Kowalski", 24);
+Employee employee2 = new Employee("Karol", "Iksiński", 36);
+Employee employee3 = new Employee("Jan", "Jankowski", 18);
+
+employee1.AddScore(5);
+employee1.AddScore(2);
+employee1.AddScore(1);
+employee1.AddScore(7);
+employee1.AddScore(3);
+
+employee2.AddScore(8);
+employee2.AddScore(5);
+employee2.AddScore(9);
+employee2.AddScore(1);
+employee2.AddScore(2);
+
+employee3.AddScore(6);
+employee3.AddScore(1);
+employee3.AddScore(7);
+employee3.AddScore(3);
+employee3.AddScore(4);
+
+var result1 = employee1.Result;
+var result2 = employee2.Result;
+var result3 = employee3.Result;
+
+var maxResult = 0;
+Employee employeeWithMaxResult = null;
+
+List<Employee> employees = new List<Employee>()
 {
-    public static string GameName = "Diablo";
+    employee1, employee2, employee3 
+};
 
-    private List<int> score = new List<int>();
-    public User(string login, string password)
+foreach (var employee in employees)
+{
+if (employee.Result > maxResult)
     {
-        this.Login = login;
-        this.Password = password;
-    }
-
-    public string Login { get; private set; }
-    public string Password { get; private set; }
-
-    public int Result
-    {
-        get
-        {
-            return this.score.Sum();
-        }
-    }
-    public void AddScore(int number)
-    {
-        this.score.Add(number);
+        employeeWithMaxResult = employee;
+        maxResult = employee.Result;
     }
 }
+
+Console.WriteLine("Zwycięzcą został " + employeeWithMaxResult.Name + " " + employeeWithMaxResult.Surname + " " 
+    + employeeWithMaxResult.Age + " lat zdobywając punktów: " + maxResult);
+Console.WriteLine("\nLista wszystkich uczestników konkursu:");
+Console.WriteLine("- " + employee1.Name + " " + employee1.Surname + " zdobył punktów: " + result1);
+Console.WriteLine("- " + employee2.Name + " " + employee2.Surname + " zdobył punktów: " + result2);
+Console.WriteLine("- " + employee3.Name + " " + employee3.Surname + " zdobył punktów: " + result3);
+
+
