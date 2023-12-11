e13259d9a455616f283c4b26194047fb268d7cd1
Anna Lukaszczyk <normalnie4@tlen.pl>
2023-12-11 11:44:03 +0100

    mój test

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,69 +1,39 @@
-﻿int number = 497248;
-string numberAsString = number.ToString();
-char[] letters = numberAsString.ToCharArray();
+﻿User user1 = new User("Ania", "sasdf");
+User user2 = new User("Marek", "sasdf");
 
-int number0 = 0;
-int number1 = 0;
-int number2 = 0;
-int number3 = 0;
-int number4 = 0;
-int number5 = 0;
-int number6 = 0;
-int number7 = 0;
-int number8 = 0;
-int number9 = 0;
-
-foreach (char letter in letters)
+user1.AddScore(5);
+user1.AddScore(2);
+user2.AddScore(3);
+user2.AddScore(6);
+var result1 = user1.Result;
+var result2 = user2.Result;
+Console.WriteLine("Wynik dla user1: " + result1);
+Console.WriteLine("Wynik dla user2: " + result2);
+var name = User.GameName;
+Console.WriteLine(name);
+class User
 {
-    if (letter == '0')
+    public static string GameName = "Diablo";
+
+    private List<int> score = new List<int>();
+    public User(string login, string password)
     {
-        number0 = number0 + 1;
+        this.Login = login;
+        this.Password = password;
     }
-    else if (letter == '1')
+
+    public string Login { get; private set; }
+    public string Password { get; private set; }
+
+    public int Result
     {
-        number1 = number1 + 1;
+        get
+        {
+            return this.score.Sum();
+        }
     }
-    else if (letter == '2')
+    public void AddScore(int number)
     {
-        number2 = number2 + 1;
-    }
-    else if (letter == '3')
-    {
-        number3 = number3 + 1;
-    }
-    else if (letter == '4')
-    {
-        number4 = number4 + 1;
-    }
-    else if (letter == '5')
-    {
-        number5 = number5 + 1;
-    }
-    else if (letter == '6')
-    {
-        number6 = number6 + 1;
-    }
-    else if (letter == '7')
-    {
-        number7 = number7 + 1;
-    }
-    else if (letter == '8')
-    {
-        number8 = number8 + 1;
-    }
-    else if (letter == '9')
-    {
-        number9 = number9 + 1;
+        this.score.Add(number);
     }
 }
-
-Console.WriteLine("Ilość wystąpień cyfry 0 we wskazanej liczbie: " + number0);
-Console.WriteLine("Ilość wystąpień cyfry 1 we wskazanej liczbie: " + number1);
-Console.WriteLine("Ilość wystąpień cyfry 2 we wskazanej liczbie: " + number2);
-Console.WriteLine("Ilość wystąpień cyfry 3 we wskazanej liczbie: " + number3);
-Console.WriteLine("Ilość wystąpień cyfry 4 we wskazanej liczbie: " + number4);
-Console.WriteLine("Ilość wystąpień cyfry 5 we wskazanej liczbie: " + number5);
-Console.WriteLine("Ilość wystąpień cyfry 6 we wskazanej liczbie: " + number6);
-Console.WriteLine("Ilość wystąpień cyfry 7 we wskazanej liczbie: " + number7);
-Console.WriteLine("Ilość wystąpień cyfry 8 we wskazanej liczbie: " + number8);
-Console.WriteLine("Ilość wystąpień cyfry 9 we wskazanej liczbie: " + number9);
